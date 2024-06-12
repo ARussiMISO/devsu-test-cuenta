745e955238b327d9bbdad283b2dab0fd8a2e445d
Fredy Alejandro Russi Fajardo <frussi@asesoftware.com>
2024-06-11 23:35:21 -0500

    Commit inicial

---
+++ src/main/java/com/devsu/cuenta/CuentaApplication.java
@@ -0,0 +1,13 @@
+package com.devsu.cuenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsu/cuenta/CuentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsu.cuenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
